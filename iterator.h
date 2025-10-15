063c173b6f7c7f5f798b1fa5064019c66867db29
Dominik Chrobak <dominikchrobak50@gmail.com>
2025-10-15 16:57:32 +0200

    Zdefiniowana klasa iteratora

---
+++ iterator.h
@@ -0,0 +1,45 @@
+// Iterator.h
+#pragma once
+#include "node.h"
+
+/**
+ * @file Iterator.h
+ * @brief Definicja klasy Iterator do przechodzenia po liœcie.
+ */
+
+template <typename T>
+class Iterator {
+private:
+    Node<T>* currentNode; // WskaŸnik na aktualny wêze³
+
+public:
+    /**
+     * @brief Konstruktor iteratora.
+     * @param node Wêze³, na który ma wskazywaæ iterator.
+     */
+    Iterator(Node<T>* node);
+
+    /**
+     * @brief Przesuwa iterator do nastêpnego elementu.
+     * @return Referencja do iteratora po przesuniêciu.
+     */
+    Iterator<T>& next();
+
+    /**
+     * @brief Przesuwa iterator do poprzedniego elementu.
+     * @return Referencja do iteratora po przesuniêciu.
+     */
+    Iterator<T>& prev();
+
+    /**
+     * @brief Zwraca dane z aktualnego wêz³a.
+     * @return Dane typu T.
+     */
+    T currentItem();
+
+    /**
+     * @brief Sprawdza, czy iterator zakoñczy³ przechodzenie po liœcie.
+     * @return True, jeœli iterator wskazuje na nullptr.
+     */
+    bool isDone();
+};
